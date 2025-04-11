9222ad2580825a6fc8d8728d505e7a812c3e4d50
Valeria Salas <37188390+visalasc@users.noreply.github.com>
2025-04-11 11:02:20 -0400

    Delete Ayudantias/Ayudantia03 directory

--- Ayudantias/Ayudantia03/src/models/board.js
+++
@@ -1,8 +0,0 @@
-const cell = require('./cell');
-
-const board = {
-    id: 1,
-    cells: cell
-}
-
-module.exports = board;
--- Ayudantias/Ayudantia03/src/models/player.js
+++
@@ -1,8 +0,0 @@
-const players = [
-    { id: 1, name: "Player 1" },
-    { id: 2, name: "Player 2" },
-    { id: 3, name: "Player 3" },
-    { id: 4, name: "Player 4" }
-];
-
-module.exports = players;
